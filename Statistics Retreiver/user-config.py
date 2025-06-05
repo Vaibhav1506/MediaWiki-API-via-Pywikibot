84ec2397f95729c65d24f5aa41b5672a2df5bcb8
Tricky Biggle <bvaibhav1506@gmail.com>
2025-06-05 14:56:42 +0530

    Update user-config.py

--- Statistics Retreiver/user-config.py
+++ Statistics Retreiver/user-config.py
@@ -1,4 +1,4 @@
-family = 'downtown1930smafia' #Name of the family wiki or the destination wiki. In this case its Downtown 1930s Mafia Wiki
-mylang = 'en'
+family = 'downtown1930smafia' #Name of the family wiki or the destination wiki. In this case its Downtown 1930s Mafia Wiki.
+mylang = 'en'   #Encoding language
 usernames['downtown1930smafia']['en'] = 'YourBotUsername' #Your Bot Username and the family name with the encoding language in this case en for english
 password_file = "user-password.py" #The password file
